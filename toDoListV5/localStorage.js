4450347601b0763e10941c19b98691d941e3af3d
Traceton Timmerman <traceton.timmerman@gmail.com>
2020-07-10 03:50:20 -0400

    Started to implement local storage.

--- toDoListV5/localStorage.js
+++ toDoListV5/localStorage.js
@@ -1,3 +1,17 @@
 export let sendToLocalStorage = (userInput) => {
   console.log(userInput);
+  localStorage.setItem(userInput, userInput);
+};
+
+export let clearLocalStorage = () => {
+  localStorage.clear();
+  console.log("Local storage cleared");
+};
+
+export let showLocalStorage = () => {
+  if (localStorage.length >= 1) {
+    for (let i = 0; i < localStorage.length; i++) {
+      console.log(localStorage.getItem(localStorage.key(i)));
+    }
+  }
 };
--- toDoListV5/main.js
+++ toDoListV5/main.js
@@ -1,11 +1,16 @@
 import { createListItem } from "./createListItems.js";
-import { sendToLocalStorage } from "./localStorage.js";
+import {
+  sendToLocalStorage,
+  clearLocalStorage,
+  showLocalStorage,
+} from "./localStorage.js";
 
 // start of elements below
 export let listDiv = document.getElementById("list");
 let userTextInput = document.getElementById("userListEntry");
 let userSubmitButton = document.getElementById("userAddButton");
-
+let clearLocal = document.getElementById("clearLocal");
+let showLocal = document.getElementById("showLocal");
 // end of elements above
 
 // start of event listeners
@@ -14,3 +19,6 @@ userSubmitButton.addEventListener("click", function () {
   sendToLocalStorage(userTextInput.value);
   userTextInput.value = "";
 });
+
+clearLocal.addEventListener("click", clearLocalStorage);
+showLocal.addEventListener("click", showLocalStorage);
