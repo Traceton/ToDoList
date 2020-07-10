8e8782e24d35970ee8dce9651ec556ce9072e7ee
Traceton Timmerman <traceton.timmerman@gmail.com>
2020-07-10 04:11:46 -0400

    fixed local storage sending and showing functions

--- toDoListV5/localStorage.js
+++ toDoListV5/localStorage.js
@@ -1,6 +1,15 @@
 export let sendToLocalStorage = (userInput) => {
-  console.log(userInput);
-  localStorage.setItem(userInput, userInput);
+  if (localStorage.length < 1) {
+    localStorage.setItem(userInput, userInput);
+  } else {
+    for (let i = 0; i < localStorage.length; i++) {
+      let storageItems = localStorage.getItem(localStorage.key(i));
+      localStorage.setItem(userInput, userInput);
+      if (storageItems != userInput) {
+        localStorage.setItem(userInput, userInput);
+      } else return;
+    }
+  }
 };
 
 export let clearLocalStorage = () => {
